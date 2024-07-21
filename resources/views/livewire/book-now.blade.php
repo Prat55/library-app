8dbc6132e1d2157d2a2fcfec48b802b1f8ad4d36
prat55 <pratikdesai9059@gmail.com>
2024-07-21 13:24:01 +0530

    New loader is added

--- resources/views/livewire/book-now.blade.php
+++ resources/views/livewire/book-now.blade.php
@@ -1,7 +1,13 @@
 <div class="mt-3 text-center">
     @include('frontend.message')
 
-    <button type="button" class="btn btn-sm btn-outline-primary" wire:click="bookNow()">
-        Book Now
+    <button type="button" class="btn btn-sm btn-outline-primary d-flex justify-content-center align-items-center"
+        wire:click="bookNow()">
+        <div wire:loading.remove="bookNow">
+            Book Now
+        </div>
+        <div style="height:50px;width:50px" wire:loading="bookNow">
+            <img src="{{ asset('user-assets/images/loader/loader.gif') }}" alt="loader" width="100%" height="100%">
+        </div>
     </button>
 </div>
